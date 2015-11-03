e3e2eaafcefcb28006420b745327a34c0fc51dc5
Abhrojit Mukherjee <abhrojit.mukherjee1994@gmail.com>
2015-11-04 00:05:14 +0530

    Implemented floating label

---
+++ app/src/main/java/cestemocsi/myapplication/Fragment_Contactus.java
@@ -0,0 +1,28 @@
+package cestemocsi.myapplication;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_Contactus extends Fragment {
+
+
+    public Fragment_Contactus() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contactus, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/cestemocsi/myapplication/Main_activity.java
@@ -0,0 +1,288 @@
+package cestemocsi.myapplication;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Main_activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    int menuID = 0;
+    private EditText inputName, inputPhone, inputAddress, inputHear;
+    private TextInputLayout inputLayoutName, inputLayoutPhone, inputLayoutAddress, inputLayoutHear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hint: Clicked", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if(savedInstanceState != null) {
+            menuID = savedInstanceState.getInt("menuID");
+        }
+
+        checkMenuID(menuID);
+    }
+
+    public void checkMenuID(int id) {
+        if(id == 0) {
+            swapFragments(new HomeFragment());
+        }
+        else {
+            onItemForFragmentSelected(menuID);
+        }
+    }
+
+    public void swapFragments(Fragment fragment) {
+        if(fragment != null) {
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+        else {
+            Toast.makeText(getBaseContext(), "Fragment not initiated", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        // Handle navigation view item clicks here.
+        menuID = item.getItemId();
+
+        onItemForFragmentSelected(menuID);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void onItemForFragmentSelected(int id) {
+        Fragment fragment = null;
+
+        if (id == R.id.nav_home) {
+            // Handle the home action
+            fragment = new HomeFragment();
+        } else if (id == R.id.nav_location) {
+            //Handle the location action
+        } else if (id == R.id.nav_about) {
+            //Handle the about action here
+        } else if (id == R.id.nav_contact) {
+            //Handle the contact action here
+            fragment = new Fragment_Contactus();
+        } else if (id == R.id.nav_email) {
+            //Handle the email action here
+        } else {
+            fragment = new HomeFragment();
+        }
+
+        swapFragments(fragment);
+    }
+
+    public void onBtnClick(View view){
+        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
+        inputLayoutPhone = (TextInputLayout) findViewById(R.id.input_layout_phoneno);
+        inputLayoutAddress = (TextInputLayout) findViewById(R.id.input_layout_address);
+        inputLayoutHear = (TextInputLayout) findViewById(R.id.input_layout_hear);
+        inputName = (EditText) findViewById(R.id.input_name);
+        inputPhone = (EditText) findViewById(R.id.input_phoneno);
+        inputAddress = (EditText) findViewById(R.id.input_address);
+        inputHear = (EditText) findViewById(R.id.input_hear);
+
+        inputName.addTextChangedListener(new MyTextWatcher(inputName));
+        inputPhone.addTextChangedListener(new MyTextWatcher(inputPhone));
+        inputAddress.addTextChangedListener(new MyTextWatcher(inputAddress));
+        inputHear.addTextChangedListener(new MyTextWatcher(inputHear));
+        submitForm();
+    }
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+        if (!validateName()) {
+            return;
+        }
+
+        if (!validatePhone()) {
+            return;
+        }
+
+        if (!validateAddress()) {
+            return;
+        }
+
+        if(!validateHear()){
+            return;
+        }
+
+        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateName() {
+        if (inputName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.err_msg_name));
+            requestFocus(inputName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validatePhone() {
+        String phone = inputPhone.getText().toString().trim();
+
+        if (phone.isEmpty() || !isValidPhone(phone)) {
+            inputLayoutPhone.setError(getString(R.string.err_msg_phone));
+            requestFocus(inputPhone);
+            return false;
+        } else {
+            inputLayoutPhone.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateAddress() {
+        if (inputAddress.getText().toString().trim().isEmpty()) {
+            inputLayoutAddress.setError(getString(R.string.err_msg_address));
+            requestFocus(inputAddress);
+            return false;
+        } else {
+            inputLayoutAddress.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateHear() {
+        if (inputName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.err_msg_name));
+            requestFocus(inputName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private static boolean isValidPhone(String phone) {
+        return !TextUtils.isEmpty(phone) && android.util.Patterns.PHONE.matcher(phone).matches();
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.input_name:
+                    validateName();
+                    break;
+                case R.id.input_phoneno:
+                    validatePhone();
+                    break;
+                case R.id.input_address:
+                    validateAddress();
+                    break;
+                case R.id.input_hear:
+                    validateHear();
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("menuID", menuID);
+    }
+}
